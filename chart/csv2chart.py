7cfea984c9ab52f9a5828f3fd1b05187c1b29986
karlfe <45057922+karlfe@users.noreply.github.com>
2020-10-26 21:15:27 -0400

    Create csv2chart.py
    First charting program

---
+++ chart/csv2chart.py
@@ -0,0 +1,16 @@
+import pandas as pd
+from datetime import datetime
+
+#df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/finance-charts-apple.csv')
+df = pd.read_csv('chart1.csv')
+#df['DateTime'] = df['Date'] + " " + df['Time']
+df['DateTime'] = datetime.strptime(df['Date'] + " " + df['Time'], "%m/%d/%Y %H:%M")
+
+import plotly.graph_objects as go
+fig = go.Figure(data=[go.Candlestick(x=df['DateTime'],
+                open=df['Open'],
+                high=df['High'],
+                low=df['Low'],
+                close=df['Close'])])
+
+fig.show()
